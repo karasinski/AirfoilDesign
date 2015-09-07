db4a7022fb69e1a4414d37d9a1539cb3486203db
John Karasinski <karasinski@gmail.com>
2015-09-07 02:26:26 -0700

    update run script to accept additional params

--- OpenFoam/run.py
+++ OpenFoam/run.py
@@ -1,17 +1,42 @@
 from __future__ import division, print_function
 from subprocess import call
 import argparse
+import os
+import time
+import numpy as np
 
 
 def run_case(foil, alpha):
+    # Clean and run case
     call("./Allclean")
-    call(["python scripts/NACA2STL.py", foil, alpha])
-    call(["./Allrun", foil, alpha])
+    call(["python",  "scripts/NACA2STL.py", foil, str(alpha)])
+    call(["./Allrun"])
+
+    # Save data
+    output_dir = os.getcwd() + '/output/' + foil + '/' + str(alpha)
+    data = os.getcwd() + '/airfoil_pimpleFoam/'
+    os.makedirs(output_dir)
+    time.sleep(1)
+    cmds = ['cp', '-r', data, output_dir]
+    call(cmds)
+
+
+def param_sweep(foil, start, stop, step):
+    alphas = np.arange(start, stop, step)
+
+    for alpha in alphas:
+        print("Running foil {}, alpha {}".format(foil, alpha))
+        run_case(foil, alpha)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Set the foil angle of attack and log results.")
-    parser.add_argument("alpha", type=int, help="Start angle of sweep.")
-    parser.add_argument("--foil", "-f", type=str, default="0012", help="Foil")
+    parser.add_argument("start", type=int, help="Start angle of sweep.")
+    parser.add_argument("stop", type=int, help="End angle of sweep. The sweep \
+                        does not include this value.")
+    parser.add_argument("step", nargs='?', type=int, default=1,
+                        help="Spacing between values.")
+    parser.add_argument("--foil", "-f", default="0012", help="Foil")
     args = parser.parse_args()
 
-    run_case(args.foil, args.alpha)
+    param_sweep(args.foil, args.start, args.stop, args.step)
