ae14a943711327529f1aed8c9dfaf78c4311cb65
John Karasinski <karasinski@gmail.com>
2015-09-08 21:11:05 -0700

    user can now set Reynolds (see #1) and Mach (see #2) numbers; set pimpleFoam convergence criteria

--- OpenFoam/run.py
+++ OpenFoam/run.py
@@ -6,11 +6,15 @@ import time
 import numpy as np
 
 
-def run_case(foil, alpha):
+def run_case(foil, alpha, Reynolds, U):
     # Clean and run case
+    print("Cleaning...")
     call(["./Allclean"])
-    call(["python",  "scripts/NACA2STL.py", foil, str(alpha)])
-    print('Running solution...')
+    call(["python", "scripts/NACA2STL.py", foil, str(alpha)])
+    call(["python", "scripts/initial_conditions.py",
+          "--Reynolds", str(Reynolds),
+          "--U", str(U)])
+    print("Running solution...")
     call(["./Allrun"])
 
     # Save data
@@ -21,13 +25,13 @@ def run_case(foil, alpha):
     call(['cp', '-r', data, output_dir])
 
 
-def param_sweep(foil, start, stop, step):
+def param_sweep(foil, start, stop, step, Reynolds, U):
     alphas = np.arange(start, stop, step)
     print("Running foil {}, alphas {}.".format(foil, alphas))
 
     for alpha in alphas:
         print("Running alpha {}.".format(alpha))
-        run_case(foil, alpha)
+        run_case(foil, alpha, Reynolds, U)
 
 
 if __name__ == "__main__":
@@ -38,8 +42,21 @@ if __name__ == "__main__":
     parser.add_argument("step", nargs='?', type=int, default=1,
                         help="Spacing between values.")
     parser.add_argument("--foil", "-f", default="0012", help="Foil")
+    parser.add_argument("--Reynolds", "-R", type=float, default=6e6,
+                        help="Reynolds number")
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--U", "-U", type=float, default=None, help="Velocity in m/s")
+    group.add_argument("--Mach", "-M", type=float, default=None, help="Mach number")
     args = parser.parse_args()
+
+    # If neither U nor Mach specified, set default velocity of 1 m/s
+    if args.U is None and args.Mach is None:
+        args.U = 1
+    # If U is not set and Mach is, set U to the appropriate velocity
+    if args.U is None and args.Mach is not None:
+        args.U = args.Mach * 332
 
     if args.stop is None:
         args.stop = args.start + 1
-    param_sweep(args.foil, args.start, args.stop, args.step)
+    param_sweep(args.foil, args.start, args.stop, args.step, args.Reynolds, args.U)
---
+++ OpenFoam/scripts/initial_conditions.py
@@ -0,0 +1,46 @@
+import argparse
+import os
+
+
+# Chord length
+c = 1
+
+def set_initial_conditions(U=1, pressure=0, turbulentKE=0.0096, turbulentOmega=9.8):
+    values = {'flowVelocity': '(' + "{:.4e}".format(U) + ' 0 0)',
+              'pressure': str(pressure),
+              'turbulentKE': str(turbulentKE),
+              'turbulentOmega': str(turbulentOmega)}
+
+    path = os.getcwd() + '/airfoil_simpleFoam/0.org/include/initialConditions'
+    template_path = path + '.template'
+    print("Setting initial conditions.")
+    with open(template_path) as f:
+        txt = f.read()
+    with open(path, "w") as f:
+        f.write(txt.format(**values))
+
+def set_Re(U, Re):
+    """
+    Set Reynolds number (to five significant digits) via kinematic viscosity in
+    the input files.
+    """
+    path = os.getcwd() + '/airfoil_simpleFoam/constant/transportProperties'
+    template_path = path + '.template'
+
+    nu = U*c/Re
+    print("Settings:\nReynolds={:.4e}\nVelocity=:{:.4e}\nnu={:.4e}".format(Re, U, nu))
+    nu = "{:.4e}".format(nu)
+
+    with open(template_path) as f:
+        txt = f.read()
+    with open(path, "w") as f:
+        f.write(txt.format(nu=nu))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Set the initial conditions.")
+    parser.add_argument("--Reynolds", "-R", type=float, default=6e6)
+    parser.add_argument("--U", "-U", type=float, default=1)
+    args = parser.parse_args()
+
+    set_initial_conditions(U=args.U)
+    set_Re(args.U, args.Reynolds)
